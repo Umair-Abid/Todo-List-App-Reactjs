210e688f25a0902010081119b4a1ab9fafcca64b
Muhammad Umair Abid <umairmalik0401@gmail.com>
2022-07-03 14:00:25 +0500

    phase 1:Completed some functionality

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,165 +1,55 @@
 import React, { useState } from "react";
 
 const Todo = () => {
-  const [text1, setText1] = useState("Task1");
-  const [text2, setText2] = useState("Task2");
-  const [text3, setText3] = useState("Task3");
-  const [text4, setText4] = useState("");
-  const [text5, setText5] = useState("");
-  const [text6, setText6] = useState("");
-  const [text7, setText7] = useState("");
-  const [text8, setText8] = useState("");
-  const [text9, setText9] = useState("");
-  const toggleTodo1 = () => {
-    setText1("");
-    setText4("Task1");
-  };
-
-  const toggleTodo2 = () => {
-    setText2("");
-    setText5("Task2");
-  };
-
-  const toggleTodo3 = () => {
-    setText3("");
-    setText6("Task3");
-  };
-  const toggleTodo4 = () => {
-    setText4("");
-    setText7("Task 1 Finished");
-  };
-
-  const toggleTodo5 = () => {
-    setText5("");
-    setText8("Task 2 Finished");
-  };
-
-  const toggleTodo6 = () => {
-    setText6("");
-    setText9("Task 3 Finished");
-  };
-
-  const clearTask = () => {
-    setText1("Task1");
-    setText2("Task2");
-    setText3("Task3");
-    setText4("");
-    setText5("");
-    setText6("");
-    setText7("");
-    setText8("");
-    setText9("");
-  };
-  const clearTask1 = () => {
-    setText7("");
-  };
-  const clearTask2 = () => {
-    setText8("");
-  };
-  const clearTask3 = () => {
-    setText9("");
-  };
+  const [text, setText] = useState('');
+  const [list, setList] = useState([]);
+  
+  const onChange=(e)=>{
+    setText(e.target.value)
+   };
+   const noteAdded=(e)=>{
+    e.preventDefault()
+    if(text.length !== 0){
+    setList([...list,text])
+    setText('')}
+    };
+    const noteDeleted=(id)=>{
+      console.log(id)
+      const updatelist=list.filter((item,ind)=>{
+        return ind !== id
+      })
+      setList(updatelist)
+     };
 
   return (
-    <>
-      <div className="px-6 py-12 mx-auto">
-        <h1 className="text-3xl font-bold text-center">Todo App</h1>
-
-        <div className="flex flex-wrap">
-          <div className="container w-1/3 mt-5 shadow-lg">
-            <h2 className="my-3 text-2xl font-bold">Initial Stage</h2>
-            <h2> {text1}</h2>
-            <button
-              onClick={toggleTodo1}
-              className=" my-1 text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-md"
-            >
-              Init Task
-            </button>
-            <h2> {text2}</h2>
-            <button
-              type="button"
-              onClick={toggleTodo2}
-              className="my-1 text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-md"
-            >
-              Init Task
-            </button>
-            <h2>{text3}</h2>
-            <button
-              type="button"
-              onClick={toggleTodo3}
-              className="my-1 text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-md"
-            >
-              Init Task
-            </button>
-          </div>
-
-          <div className="container w-1/3 mt-5 shadow-lg">
-            <h2 className="my-3 text-2xl font-bold">Todo App</h2>
-            <h2> {text4}</h2>
-            <button
-              type="button"
-              onClick={toggleTodo4}
-              className="my-1 text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-md"
-            >
-              Process
-            </button>
-            <h2> {text5}</h2>
-            <button
-              type="button"
-              onClick={toggleTodo5}
-              className="my-1 text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-md"
-            >
-              Process
-            </button>
-            <h2>{text6}</h2>
-            <button
-              type="button"
-              onClick={toggleTodo6}
-              className="my-1 text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-md"
-            >
-              Process
-            </button>
-          </div>
-          <div className="container w-1/3 mt-5 shadow-lg px-8">
-            <h2 className="my-3 text-2xl font-bold">Result</h2>
-
-            <h2> {text7}</h2>
-            <button
-              type="button"
-              onClick={clearTask1}
-              className=" my-1 text-white bg-indigo-500 border-0 py-1 px-5 focus:outline-none hover:bg-indigo-600 rounded text-md"
-            >
-              Clear
-            </button>
-            <h2> {text8}</h2>
-            <button
-              type="button"
-              onClick={clearTask2}
-              className=" my-1  text-white bg-indigo-500 border-0 py-1 px-5 focus:outline-none hover:bg-indigo-600 rounded text-md"
-            >
-              Clear
-            </button>
-            <h2>{text9}</h2>
-            <button
-              type="button"
-              onClick={clearTask3}
-              className="  my-1 text-white bg-indigo-500 border-0 py-1 px-5 focus:outline-none hover:bg-indigo-600 rounded text-md"
-            >
-              Clear
-            </button>
-          </div>
-        </div>
-        <div className="container text-center">
-          <button
-            type="button"
-            onClick={clearTask}
-            className="mt-8  text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-md item-center"
-          >
-            Reset All Task
-          </button>
-        </div>
-      </div>
-    </>
+    <div className="px-6 py-12 mx-auto">
+    <h1 className="text-center text-3xl font-bold" >Todo</h1>
+    
+     <form onSubmit={noteAdded}>
+    <div className="flex justify-center items-center mt-10 py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700">
+       
+        <textarea value={text} onChange={onChange}id="chat" rows="1" className="block mx-4 p-2.5 w-1/3 text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-blue-800 dark:border-blue-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+            <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+            <svg className="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
+        </button>
+    </div>
+    </form>
+<div className="mt-10">
+          {list.map((item,ind)=>{
+            return <div key={ind}>
+    <div className="flex justify-center items-center mt-10 py-2 px-3">
+       
+        <div className="text-start py-3 px-36 border rounded border-gray-300 hover:bg-blue-500">{item}</div>
+            <button onClick={()=> noteDeleted(ind)} className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+            Delete
+        </button>
+    </div>  
+    </div>  
+          })}
+          
+      
+</div>
+</div>
   );
 };
 
